872210bd4fa2c519028e30c301a5d55de6aabb65
sunbin001 <492890046@qq.com>
2019-08-19 17:59:37 +0800

    fanfan

--- src/app/app.router.module.ts
+++ src/app/app.router.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
         path:"**",redirectTo:"/pageNotFound"   
     }
 ];
- 
+ //tag1
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
